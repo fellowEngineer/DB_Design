301cd7936d5ced49c9e11c9c6a7b43c5b344b77d
fellow_engineer <71657493+fellowEngineer@users.noreply.github.com>
2024-09-12 12:07:43 +0000

    update signup controller

--- src/controllers/signup.controller.js
+++ src/controllers/signup.controller.js
@@ -4,8 +4,16 @@ const getSignUp = (req, res) => {
 }
 
 
+import DB_SignUP from "../db/signup.db.js"
 const postSignUp = (req, res) => {
-    
+    ///this is the place where i should check for allthe data present, validation, etc
+    /// then forward to create user into db
+
+
+
+    DB_SignUP()
+    .then( (reslut) => {} )
+    .catch( (error) => {} )
     
 }
 
