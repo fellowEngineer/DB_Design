57a41d21f89c5a37b1f42d88dc480c50c319a8fb
fellow_engineer <71657493+fellowEngineer@users.noreply.github.com>
2024-09-12 11:52:13 +0000

    add signup controller

---
+++ src/controllers/signup.controller.js
@@ -0,0 +1,16 @@
+const getSignUp = (req, res) => {
+    res.Status(2000)
+    .json("A HTML Page Suppose to Render here");
+}
+
+
+const postSignUp = (req, res) => {
+    
+    
+}
+
+
+export {
+    getSignUp,
+    postSignUp
+}
